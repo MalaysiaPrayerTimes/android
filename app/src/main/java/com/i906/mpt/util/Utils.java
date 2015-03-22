892fa3617e08ed259ef99b7b9fb751e4c2a2a518
i906 <ilhami.i906@gmail.com>
2015-03-23 00:33:19 +0800

    Removed stray logs

--- app/src/main/java/com/i906/mpt/util/Utils.java
+++ app/src/main/java/com/i906/mpt/util/Utils.java
@@ -3,7 +3,6 @@ package com.i906.mpt.util;
 import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
-import android.util.Log;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -23,8 +22,6 @@ public class Utils {
         Sensor a = sm.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
         Sensor b = sm.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
         Sensor c = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        boolean d = a != null || (b != null && c != null);
-        Log.d("sensors", String.format("%s %s %s :: %s", a, b, c, d));
-        return d;
+        return a != null || (b != null && c != null);
     }
 }
