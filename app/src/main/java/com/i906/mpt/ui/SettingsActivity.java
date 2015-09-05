fb4b6417464d3f8ebf24144e4ab2953145d30b56
i906 <ilhami.i906@gmail.com>
2015-09-05 22:59:29 +0800

    Added notification settings

--- app/src/main/java/com/i906/mpt/ui/SettingsActivity.java
+++ app/src/main/java/com/i906/mpt/ui/SettingsActivity.java
@@ -5,13 +5,19 @@ import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.preference.RingtonePreference;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.widget.LinearLayout;
 
@@ -43,6 +49,7 @@ public class SettingsActivity extends PreferenceActivity {
         }
 
         addPreferencesFromResource(R.xml.pref_interface);
+        addPreferencesFromResource(R.xml.pref_notifications);
         addPreferencesFromResource(R.xml.pref_about);
 
         Preference version = findPreference("general_pref_version");
@@ -50,6 +57,10 @@ public class SettingsActivity extends PreferenceActivity {
         Preference rate = findPreference("general_pref_review");
         Preference feedback = findPreference("general_pref_feedback");
         Preference screen = findPreference("general_pref_screen");
+
+        bindPreferenceSummaryToValue(findPreference("notification_appear_before_duration"));
+        bindPreferenceSummaryToValue(findPreference("notification_clear_after_duration"));
+        bindPreferenceSummaryToValue(findPreference("notification_alarm_offset"));
 
         version.setSummary(BuildConfig.VERSION_NAME);
         build.setSummary(String.format("%s %s %s",
@@ -96,6 +107,48 @@ public class SettingsActivity extends PreferenceActivity {
     @Override
     public boolean onIsMultiPane() {
         return isXLargeTablet(this) && !isSimplePreferences(this);
+    }
+
+    private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = (preference, value) -> {
+        String stringValue = value.toString();
+
+        if (preference instanceof ListPreference) {
+            ListPreference listPreference = (ListPreference) preference;
+            int index = listPreference.findIndexOfValue(stringValue);
+
+            preference.setSummary(
+                    index >= 0
+                            ? listPreference.getEntries()[index]
+                            : null);
+
+        } else if (preference instanceof RingtonePreference) {
+            if (TextUtils.isEmpty(stringValue)) {
+                preference.setSummary("Silent");
+
+            } else {
+                Ringtone ringtone = RingtoneManager.getRingtone(
+                        preference.getContext(), Uri.parse(stringValue));
+
+                if (ringtone == null) {
+                    preference.setSummary(null);
+                } else {
+                    String name = ringtone.getTitle(preference.getContext());
+                    preference.setSummary(name);
+                }
+            }
+
+        } else {
+            preference.setSummary(stringValue);
+        }
+        return true;
+    };
+
+    private static void bindPreferenceSummaryToValue(Preference preference) {
+        preference.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
+        sBindPreferenceSummaryToValueListener.onPreferenceChange(preference,
+                PreferenceManager
+                        .getDefaultSharedPreferences(preference.getContext())
+                        .getString(preference.getKey(), ""));
     }
 
     private static boolean isXLargeTablet(Context context) {
