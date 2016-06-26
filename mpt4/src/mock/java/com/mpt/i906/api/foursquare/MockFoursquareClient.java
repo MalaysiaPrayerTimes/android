ccb037a4b8facafc47bc628b46bedfa0c5948675
Noorzaini Ilhami <ilhami.i906@gmail.com>
2016-06-26 16:39:07 +0800

    Added LocationRepository

--- mpt4/src/mock/java/com/mpt/i906/api/foursquare/MockFoursquareClient.java
+++ mpt4/src/mock/java/com/mpt/i906/api/foursquare/MockFoursquareClient.java
@@ -1,12 +1,17 @@
 package com.mpt.i906.api.foursquare;
 
+import android.util.Log;
+
 import com.i906.mpt.api.foursquare.FoursquareClient;
 import com.i906.mpt.api.foursquare.Mosque;
 import com.mpt.i906.api.MockApiUtils;
 
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
 
 import rx.Observable;
+import rx.functions.Func1;
 
 /**
  * @author Noorzaini Ilhami
@@ -21,7 +26,25 @@ public class MockFoursquareClient implements FoursquareClient {
 
     @Override
     public Observable<List<Mosque>> getMosqueList(double lat, double lng, int radius) {
-        List<Mosque> mosqueList = mUtils.getDataList(Mosque.class, "json/mosque.json");
-        return Observable.just(mosqueList);
+        Log.w("MockFoursquareClient", "Performing network tasks. Lat: " + lat + " Lng: " + lng);
+        return Observable.just(randomInt(0, 10))
+                .delay(randomInt(0, 5000), TimeUnit.MILLISECONDS)
+                .flatMap(new Func1<Integer, Observable<List<Mosque>>>() {
+                    @Override
+                    public Observable<List<Mosque>> call(Integer r) {
+                        if (r % 4 == 0) {
+                            Log.w("MockFoursquareClient", "Returning error.");
+                            return Observable.error(new RuntimeException("Random error."));
+                        } else {
+                            Log.w("MockFoursquareClient", "Returning data.");
+                            List<Mosque> mosqueList = mUtils.getDataList(Mosque.class, "json/mosque.json");
+                            return Observable.just(mosqueList);
+                        }
+                    }
+                });
+    }
+
+    private int randomInt(int min, int max) {
+        return ThreadLocalRandom.current().nextInt(min, max + 1);
     }
 }
