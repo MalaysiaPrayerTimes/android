a042bd225e1fad9d06d1dc81ac3cc391e3675d2f
Noorzaini Ilhami <ilhami.i906@gmail.com>
2016-07-04 13:54:08 +0800

    Add barebones PrayerFragment

--- mpt4/src/main/java/com/i906/mpt/prayer/PrayerFragment.java
+++ mpt4/src/main/java/com/i906/mpt/prayer/PrayerFragment.java
@@ -1,11 +1,60 @@
 package com.i906.mpt.prayer;
 
-import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.i906.mpt.R;
+import com.i906.mpt.common.BaseFragment;
+
+import javax.inject.Inject;
 
 /**
  * @author Noorzaini Ilhami
  */
-public class PrayerFragment extends Fragment {
+public class PrayerFragment extends BaseFragment implements PrayerView {
 
+    @Inject
+    PrayerPresenter mPresenter;
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activityGraph().inject(this);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_prayer, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mPresenter.setView(this);
+        mPresenter.getPrayerContext(false);
+    }
+
+    @Override
+    public void showPrayerContext(PrayerContext prayerContext) {
+        Prayer current = prayerContext.getCurrentPrayer();
+        Log.d("PrayerFragment", "currentPrayer: " + current);
+    }
+
+    @Override
+    public void showError(Throwable error) {
+    }
+
+    @Override
+    public void showLoading() {
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mPresenter.setView(null);
+    }
 }
