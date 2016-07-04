f4902445cd9f7f694fd05737514eaf75a7161379
Noorzaini Ilhami <ilhami.i906@gmail.com>
2016-07-04 12:01:12 +0800

    Make MockPrayerClient give same day prayer times

--- mpt4/src/mock/java/com/mpt/i906/api/prayer/MockPrayerClient.java
+++ mpt4/src/mock/java/com/mpt/i906/api/prayer/MockPrayerClient.java
@@ -2,8 +2,11 @@ package com.mpt.i906.api.prayer;
 
 import com.i906.mpt.api.prayer.PrayerClient;
 import com.i906.mpt.api.prayer.PrayerData;
+import com.i906.mpt.date.DateTimeHelper;
 import com.mpt.i906.api.MockApiUtils;
 
+import java.util.Date;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import rx.Observable;
@@ -15,13 +18,15 @@ import rx.functions.Func1;
 public class MockPrayerClient implements PrayerClient {
 
     private final MockApiUtils mUtils;
+    private final DateTimeHelper mDateHelper;
 
-    public MockPrayerClient(MockApiUtils utils) {
+    public MockPrayerClient(MockApiUtils utils, DateTimeHelper date) {
         mUtils = utils;
+        mDateHelper = date;
     }
 
     @Override
-    public Observable<PrayerData> getPrayerTimesByCode(String code, int year, int month) {
+    public Observable<PrayerData> getPrayerTimesByCode(String code, final int year, final int month) {
         return Observable.just(mUtils.randomInt(0, 10))
                 .delay(mUtils.randomInt(0, 5000), TimeUnit.MILLISECONDS)
                 .flatMap(new Func1<Integer, Observable<PrayerData>>() {
@@ -31,6 +36,17 @@ public class MockPrayerClient implements PrayerClient {
                             return Observable.error(new RuntimeException("Random error."));
                         } else {
                             PrayerData data = mUtils.getData(PrayerData.class, "json/prayer.json");
+
+                            List<List<Date>> dailyDates = data.getPrayerTimes();
+
+                            for (List<Date> dates : dailyDates) {
+                                for (Date date : dates) {
+                                    date.setDate(mDateHelper.getCurrentDate());
+                                    date.setMonth(month);
+                                    date.setYear(year);
+                                }
+                            }
+
                             return Observable.just(data);
                         }
                     }
