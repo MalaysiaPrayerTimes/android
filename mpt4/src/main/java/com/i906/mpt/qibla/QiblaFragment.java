26e357710eded7576d1598ad333bc4a19c5518e6
Noorzaini Ilhami <ilhami.i906@gmail.com>
2016-07-16 21:26:25 +0800

    Add CompassView

--- mpt4/src/main/java/com/i906/mpt/qibla/QiblaFragment.java
+++ mpt4/src/main/java/com/i906/mpt/qibla/QiblaFragment.java
@@ -1,9 +1,20 @@
 package com.i906.mpt.qibla;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.i906.mpt.R;
 
 /**
  * @author Noorzaini Ilhami
  */
 public class QiblaFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_qibla, container, false);
+    }
 }
