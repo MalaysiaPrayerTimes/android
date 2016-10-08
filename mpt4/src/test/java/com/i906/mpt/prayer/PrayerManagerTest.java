d3cd8873c539504e06c13c70eb1e9db35da6ee6a
Noorzaini Ilhami <ilhami.i906@gmail.com>
2016-10-08 14:42:43 +0800

    Fix some tests

--- mpt4/src/test/java/com/i906/mpt/prayer/PrayerManagerTest.java
+++ mpt4/src/test/java/com/i906/mpt/prayer/PrayerManagerTest.java
@@ -7,6 +7,7 @@ import com.i906.mpt.api.prayer.PrayerClient;
 import com.i906.mpt.api.prayer.PrayerData;
 import com.i906.mpt.date.DateTimeHelper;
 import com.i906.mpt.location.LocationRepository;
+import com.i906.mpt.prefs.HiddenPreferences;
 import com.i906.mpt.prefs.InterfacePreferences;
 
 import org.junit.Before;
@@ -21,6 +22,8 @@ import rx.Observable;
 import rx.observers.TestSubscriber;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyBoolean;
 import static org.mockito.Matchers.anyDouble;
 import static org.mockito.Matchers.anyInt;
 import static org.mockito.Matchers.eq;
@@ -43,6 +46,9 @@ public class PrayerManagerTest {
     private LocationRepository mLocationRepository;
 
     @Mock
+    private PrayerCacheManager mPrayerCache;
+
+    @Mock
     private PrayerClient mPrayerClient;
 
     @Mock
@@ -54,6 +60,12 @@ public class PrayerManagerTest {
     @Mock
     private InterfacePreferences mInterfacePreferences;
 
+    @Mock
+    private PrayerBroadcaster mPrayerBroadcaster;
+
+    @Mock
+    private HiddenPreferences mHiddenPreferences;
+
     @Before
     public void setup() {
         MockitoAnnotations.initMocks(this);
@@ -61,14 +73,27 @@ public class PrayerManagerTest {
 
     @Test
     public void getPrayerContextSameYear() {
-        PrayerManager prayerManager = new PrayerManager(mDateHelper, mInterfacePreferences,
-                mLocationRepository, mPrayerClient);
+        when(mHiddenPreferences.getLocationDistanceLimit())
+                .thenReturn(5000L);
+
+        PrayerManager prayerManager = new PrayerManager(
+                mDateHelper,
+                mInterfacePreferences,
+                mLocationRepository,
+                mPrayerCache,
+                mPrayerClient,
+                mPrayerBroadcaster,
+                mHiddenPreferences
+        );
 
         when(mLocation.getLatitude())
                 .thenReturn(3.28011);
 
         when(mLocation.getLongitude())
                 .thenReturn(101.556);
+
+        when(mLocation.distanceTo(any(Location.class)))
+                .thenReturn(0f);
 
         when(mDateHelper.getCurrentMonth())
                 .thenReturn(3);
@@ -82,8 +107,11 @@ public class PrayerManagerTest {
         when(mDateHelper.isNextMonthNewYear())
                 .thenReturn(false);
 
-        when(mLocationRepository.getLocation())
+        when(mLocationRepository.getLocation(anyBoolean()))
                 .thenReturn(Observable.just(mLocation));
+
+        when(mPrayerCache.get(anyInt(), anyInt(), eq(mLocation)))
+                .thenReturn(Observable.<PrayerData>empty());
 
         when(mPrayerClient.getPrayerTimesByCoordinates(anyDouble(), anyDouble(), anyInt(), anyInt()))
                 .thenReturn(Observable.just(mPrayerData));
@@ -109,14 +137,27 @@ public class PrayerManagerTest {
 
     @Test
     public void getPrayerContextDifferentYear() {
-        PrayerManager prayerManager = new PrayerManager(mDateHelper, mInterfacePreferences,
-                mLocationRepository, mPrayerClient);
+        when(mHiddenPreferences.getLocationDistanceLimit())
+                .thenReturn(5000L);
+
+        PrayerManager prayerManager = new PrayerManager(
+                mDateHelper,
+                mInterfacePreferences,
+                mLocationRepository,
+                mPrayerCache,
+                mPrayerClient,
+                mPrayerBroadcaster,
+                mHiddenPreferences
+        );
 
         when(mLocation.getLatitude())
                 .thenReturn(3.28011);
 
         when(mLocation.getLongitude())
                 .thenReturn(101.556);
+
+        when(mLocation.distanceTo(any(Location.class)))
+                .thenReturn(0f);
 
         when(mDateHelper.getCurrentMonth())
                 .thenReturn(11);
@@ -133,8 +174,11 @@ public class PrayerManagerTest {
         when(mDateHelper.isNextMonthNewYear())
                 .thenReturn(true);
 
-        when(mLocationRepository.getLocation())
+        when(mLocationRepository.getLocation(anyBoolean()))
                 .thenReturn(Observable.just(mLocation));
+
+        when(mPrayerCache.get(anyInt(), anyInt(), eq(mLocation)))
+                .thenReturn(Observable.<PrayerData>empty());
 
         when(mPrayerClient.getPrayerTimesByCoordinates(anyDouble(), anyDouble(), anyInt(), anyInt()))
                 .thenReturn(Observable.just(mPrayerData));
