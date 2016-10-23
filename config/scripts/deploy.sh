150526b1069f9d281c871b980911b0661823ce8b
Noorzaini Ilhami <ilhami.i906@gmail.com>
2016-10-23 14:20:01 +0800

    Update travis YML

--- config/scripts/deploy.sh
+++ config/scripts/deploy.sh
@@ -1,7 +1,7 @@
 if [ "$TRAVIS_TAG" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
     ./gradlew generateProdReleasePlayResources
     # ./gradlew publishListingProdRelease -PPLAY_STORE_TRACK=rollout
-elif [ "$TRAVIS_BRANCH" == "develop" ]; then
+elif [ "$TRAVIS_BRANCH" == "dev" ]; then
     ./gradlew generateBetaReleasePlayResources
     # ./gradlew publishListingBetaRelease
 fi
