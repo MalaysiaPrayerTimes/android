490227ae75d26ee14b10412fc4db12e83ecfe832
Noorzaini Ilhami <ilhami.i906@gmail.com>
2016-10-23 14:54:07 +0800

    Fix some beta and prod flavour issues

--- app/src/beta/java/com/i906/mpt/internal/ApiModule.java
+++ app/src/beta/java/com/i906/mpt/internal/ApiModule.java
@@ -1,17 +1,14 @@
 package com.i906.mpt.internal;
 
-import com.i906.mpt.BuildConfig;
+import android.content.Context;
+
 import com.i906.mpt.api.foursquare.FoursquareClient;
-import com.i906.mpt.api.foursquare.FoursquareHttpInterceptor;
-import com.i906.mpt.api.foursquare.RetrofitFoursquareClient;
 import com.i906.mpt.api.prayer.PrayerClient;
-import com.i906.mpt.api.prayer.RetrofitPrayerClient;
 
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
-import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 
 /**
@@ -23,19 +20,12 @@ class ApiModule {
     @Provides
     @Singleton
     PrayerClient providePrayerClient(OkHttpClient client) {
-        OkHttpClient.Builder mptClient = client.newBuilder();
-        return new RetrofitPrayerClient(mptClient.build());
+        return BaseApiModule.getPrayerClient(client);
     }
 
     @Provides
     @Singleton
-    FoursquareClient provideFoursquareClient(OkHttpClient client) {
-        Interceptor i = new FoursquareHttpInterceptor(BuildConfig.FOURSQUARE_ID,
-                BuildConfig.FOURSQUARE_SECRET);
-
-        OkHttpClient.Builder foursquareClient = client.newBuilder();
-        foursquareClient.interceptors().add(0, i);
-
-        return new RetrofitFoursquareClient(foursquareClient.build());
+    FoursquareClient provideFoursquareClient(OkHttpClient client, Context context) {
+        return BaseApiModule.getFoursquareClient(client, context);
     }
 }
--- app/src/prod/java/com/i906/mpt/internal/ApiModule.java
+++ app/src/prod/java/com/i906/mpt/internal/ApiModule.java
@@ -2,18 +2,13 @@ package com.i906.mpt.internal;
 
 import android.content.Context;
 
-import com.i906.mpt.R;
 import com.i906.mpt.api.foursquare.FoursquareClient;
-import com.i906.mpt.api.foursquare.FoursquareHttpInterceptor;
-import com.i906.mpt.api.foursquare.RetrofitFoursquareClient;
 import com.i906.mpt.api.prayer.PrayerClient;
-import com.i906.mpt.api.prayer.RetrofitPrayerClient;
 
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
-import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 
 /**
@@ -25,21 +20,12 @@ class ApiModule {
     @Provides
     @Singleton
     PrayerClient providePrayerClient(OkHttpClient client) {
-        OkHttpClient.Builder mptClient = client.newBuilder();
-        return new RetrofitPrayerClient(mptClient.build());
+        return BaseApiModule.getPrayerClient(client);
     }
 
     @Provides
     @Singleton
     FoursquareClient provideFoursquareClient(OkHttpClient client, Context context) {
-        String fid = context.getString(R.string.foursquare_id);
-        String fsc = context.getString(R.string.foursquare_secret);
-
-        Interceptor i = new FoursquareHttpInterceptor(fid, fsc);
-
-        OkHttpClient.Builder foursquareClient = client.newBuilder();
-        foursquareClient.interceptors().add(0, i);
-
-        return new RetrofitFoursquareClient(foursquareClient.build());
+        return BaseApiModule.getFoursquareClient(client, context);
     }
 }
