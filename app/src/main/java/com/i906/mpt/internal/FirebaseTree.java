3495d3305060fde31aed8315582851185c5fbd7e
Noorzaini Ilhami <ilhami.i906@gmail.com>
2016-10-30 19:42:31 +0800

    Fix compile errors if keys not present

--- app/src/main/java/com/i906/mpt/internal/FirebaseTree.java
+++ app/src/main/java/com/i906/mpt/internal/FirebaseTree.java
@@ -1,5 +1,6 @@
 package com.i906.mpt.internal;
 
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.crash.FirebaseCrash;
 
 /**
@@ -9,6 +10,13 @@ public class FirebaseTree extends BaseMptTree {
 
     @Override
     protected void log(int priority, String tag, String message, Throwable t) {
+        try {
+            FirebaseApp.getInstance();
+        } catch (IllegalStateException e) {
+            // Skip logging if Firebase not initialized
+            return;
+        }
+
         if (t != null) {
             if (!shouldReport(t)) return;
             FirebaseCrash.report(t);
